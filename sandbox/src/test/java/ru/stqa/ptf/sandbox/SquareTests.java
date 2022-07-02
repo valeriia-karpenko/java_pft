b939fe73a0d60bc55cd326a88fd34714616e95c9
VKarpenko <Valeriia.Karpenko@nexign.com>
2022-07-02 20:58:46 +0300

    Растояние между точками p1 и p2

---
+++ sandbox/src/test/java/ru/stqa/ptf/sandbox/SquareTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.ptf.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.sandbox.Square;
+
+public class SquareTests {
+
+  @Test
+  public void testArea() {
+    Square s = new Square(5);
+    Assert.assertEquals(s.area(), 25);
+  }
+}
