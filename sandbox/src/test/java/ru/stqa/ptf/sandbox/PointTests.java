1c6a773e1a3862d75acbc1c7764eb318af1c3277
VKarpenko <Valeriia.Karpenko@nexign.com>
2022-07-04 11:20:34 +0300

    задание 3

--- sandbox/src/test/java/ru/stqa/ptf/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/ptf/sandbox/PointTests.java
@@ -6,9 +6,23 @@ import ru.stqa.pft.sandbox.Point;
 
 public class PointTests {
   @Test
-  public void testDistance() {
+  public void testDistance1() {
     Point p1 = new Point(0.0, 3.0);
     Point p2 = new Point(2.0, 3.0);
+    Assert.assertEquals(p2.distance(p1), 2.0);
+  }
+
+  @Test
+  public void testDistance2() {
+    Point p1 = new Point(0.0, 0.0);
+    Point p2 = new Point(0.0, 0.0);
+    Assert.assertEquals(p2.distance(p1), 0.0);
+  }
+
+  @Test
+  public void testDistance3() {
+    Point p1 = new Point(0.0, -3.0);
+    Point p2 = new Point(-2.0, -3.0);
     Assert.assertEquals(p2.distance(p1), 2.0);
   }
 }
