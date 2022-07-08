cd000e0c5753351d9d784bb5142a891a3d1e06c5
VKarpenko <Valeriia.Karpenko@nexign.com>
2022-07-08 09:19:40 +0300

    добавлен класс ContactHelper

---
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,50 @@
+package ru.stqa.ptf.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import ru.stqa.ptf.addressbook.model.ContactData;
+
+public class ContactHelper extends HelperBase{
+
+  public ContactHelper(WebDriver wd) {
+    super(wd);
+  }
+  
+  public void selectContact() {
+    click(By.name("selected[]"));
+  }
+
+  public void goToHomePage() {
+   click(By.linkText("home"));
+  }
+
+  public void submitContactCreation() {
+    click(By.xpath("//div[@id='content']/form/input[21]"));
+  }
+
+  public void fillContactForm(ContactData contactData) {
+    type(By.name("firstname"), contactData.getName());
+    type(By.name("middlename"), contactData.getMiddle_name());
+    type(By.name("lastname"), contactData.getLast_name());
+    type(By.name("nickname"), contactData.getNickname());
+    type(By.name("address"), contactData.getAddress());
+    type(By.name("mobile"), contactData.getMobile());
+    type(By.name("email"), contactData.getEmail());
+  }
+
+  public void initNewContactPage() {
+    click(By.linkText("add new"));
+  }
+
+  public void deleteSelectedContacts() {
+    click(By.xpath("//input[@value='Delete']"));
+  }
+
+  public void initContactModification() {
+    click(By.xpath("//img[@alt='Edit']"));
+  }
+
+  public void updateContactModification() {
+    click(By.name("update"));
+  }
+}
