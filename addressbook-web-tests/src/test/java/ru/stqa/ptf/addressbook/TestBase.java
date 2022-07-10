33ca0253373fe2aa4b6ecbbd249c8ab05c53329d
VKarpenko <Valeriia.Karpenko@nexign.com>
2022-07-10 17:35:22 +0300

    запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/TestBase.java
@@ -1,19 +1,20 @@
 package ru.stqa.ptf.addressbook;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.ptf.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-  @BeforeMethod(alwaysRun = true)
+  @BeforeMethod
   public void setUp() throws Exception{
     app.init();
   }
 
-  @AfterMethod(alwaysRun = true)
+  @AfterMethod
   public void tearDown() {
     app.stop();
   }
--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package ru.stqa.ptf.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +15,20 @@ public class ApplicationManager {
 
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver();
+    } else if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.IE) {
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     // login
     wd.get("http://localhost/addressbook/group.php");
