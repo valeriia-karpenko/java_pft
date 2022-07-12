8ff3c0427e68a24c6c0b0f219cef07527015a3e8
VKarpenko <Valeriia.Karpenko@nexign.com>
2022-07-12 16:49:18 +0300

    реализована проверка и обеспечение выполнения условий

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupDeletionTests.java
@@ -2,12 +2,16 @@ package ru.stqa.ptf.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.TestBase;
+import ru.stqa.ptf.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
