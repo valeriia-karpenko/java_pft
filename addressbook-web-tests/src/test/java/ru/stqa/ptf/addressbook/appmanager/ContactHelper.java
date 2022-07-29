ac5907f3a5c69c9c15b8f5eb5265c620a5846749
VKarpenko <Valeriia.Karpenko@nexign.com>
2022-07-29 10:56:39 +0300

    исправление по заданию 8

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ContactHelper.java
@@ -30,7 +30,7 @@ public class ContactHelper extends HelperBase{
     type(By.name("email"), contactData.getEmail());
 
     if (creation) {
-      new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
+      new Select(wd.findElement(By.name("new_group"))).selectByIndex(0);
     } else {
       Assert.assertFalse(isElementPresent(By.name("new_group")));
     }
